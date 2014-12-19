b4af634ccc410d34c89a39b543180c1e501a3150
Lance Gatlin <lance.gatlin@gmail.com>
2014-12-19 14:00:50 -0500

    refactored impl

--- src/main/scala/org/lancegatlin/Macro.scala
+++ src/main/scala/org/lancegatlin/Macro.scala
@@ -24,16 +24,16 @@ object Macro {
     val printableTypeConstructor = typeOf[Printable[_]].typeConstructor
     val aType = c.weakTypeOf[A]
 
-    val oomArg = oomNormArg(c)(aType)
+    val oomTypeSig = calcTypeSig(c)(aType)
 
     val values =
-      if(oomArg.size == 1) {
-        val (symbol, _type) = oomArg.head
+      if(oomTypeSig.size == 1) {
+        val (symbol, _type) = oomTypeSig.head
         val innerPrintableType = appliedType(printableTypeConstructor, List(_type))
         val innerPrintable = inferImplicitOrDie(c)(innerPrintableType)
         q"""${symbol.name.toString} + "=" + $innerPrintable.print(tuple)""" :: Nil
       } else {
-        oomArg
+        oomTypeSig
           .zipWithIndex
           .map { case ((symbol, _type), i) =>
             val innerPrintableType = appliedType(printableTypeConstructor, List(_type))
@@ -57,26 +57,6 @@ new Printable[$aType] {
   }
 
 
-
-  /**
-   * Get the "normalized" arguments for a type. The argument types are extracted
-   * from the unapply method. The argument names are extracted from the apply
-   * method whose argument types match the unapply method.
-   * @param c context
-   * @param aType type to compute arguments for
-   * @return non-empty list of argument (Symbol,Type) pairs
-   */
-  def oomNormArg(
-    c: blackbox.Context
-  )(
-    aType: c.Type
-  ) : List[(c.Symbol, c.Type)] = {
-    val (applyMethod, unapplyMethod) = getApplyUnapplyMethodsOrDie(c)(aType)
-    val oomType = oomNormType(c)(unapplyMethod)
-    val oomApplyMethodArg : List[c.Symbol] = applyMethod.paramLists.head
-    oomApplyMethodArg.zip(oomType)
-  }
-
   /**
    * Infer an implicit value for a type. Aborts if the implicit value does not
    * exist.
@@ -97,127 +77,129 @@ new Printable[$aType] {
   }
 
   /**
-   * Get a symbol to the apply method for a type's companion object within a 
-   * context. Aborts if apply doesn't exist.
+   * Get a symbol to a method in a type's companion object within a
+   * context. Aborts if method doesn't exist.
    * @param c context
-   * @param aType type whose companion object should be searched for apply 
-   *              method
-   * @return a method symbol to the apply method
+   * @param aType type whose companion object should be searched for method
+   * @return the matching method symbol
    */
-  def getApplyMethodOrDie(
+  def getCompanionMethodOrDie(
     c: blackbox.Context
   )(
-    aType: c.Type
+    aType: c.Type,
+    methodName: String
   ) : c.universe.MethodSymbol = {
     import c.universe._
-    aType.typeSymbol.companion.typeSignature.decl(TermName("apply")) match {
+    aType.typeSymbol.companion.typeSignature.decl(TermName(methodName)) match {
       case NoSymbol => 
         c.abort(
           c.enclosingPosition,
-          s"$aType.apply method does not exist"
+          s"$aType.$methodName method does not exist"
         )
       case s => s.asMethod
     }
-  }
-
-  /**
-   * Get a symbol to the unapply method for a type's companion object within a 
-   * context. Aborts if unapply doesn't exist
-   * @param c context
-   * @param aType type whose companion object should be searched for unapply 
-   *              method
-   * @return a method symbol to the unapply method
-   */
-  def getUnapplyMethodOrDie(
-    c: blackbox.Context
-  )(
-    aType: c.Type
-  ): c.universe.MethodSymbol= {
-    import c.universe._
-    aType.typeSymbol.companion.typeSignature.decl(TermName("unapply")) match {
-      case NoSymbol => 
-        c.abort(
-          c.enclosingPosition,
-          s"$aType.unapply does not exist"
-        )
-      case s => s.asMethod
-    }
-  }
-
-  /**
-   * Get the apply/unapply method pair for a type's companion object within a
-   * context. The type's companion object apply methods are searched for the 
-   * first apply method whose type arguments match the types of the inner types 
-   * returned by the unapply method(Option[(inner1,inner2,...)]). Aborts if 
-   * either apply or unapply don't exist or if none of the apply methods "match"
-   * the unapply method.
-   * @param c context
-   * @param aType type whose companion object should be searched for apply and
-   *              unapply methods
-   * @return a pair (apply method, unapply method) where the apply and unapply
-   *         methods "match".
-   */
-  def getApplyUnapplyMethodsOrDie(
-    c: blackbox.Context
-  )(
-    aType: c.Type
-  ): (c.universe.MethodSymbol, c.universe.MethodSymbol) = {
-    import c.universe._
-    val unapplyMethod = getUnapplyMethodOrDie(c)(aType)
-
-    val oomApplyAlt =
-      getApplyMethodOrDie(c)(aType).asTerm.alternatives.map(_.asMethod)
-    val oomUnapplyType = oomNormType(c)(unapplyMethod)
-    val optApplyMethod = oomApplyAlt.find { alt =>
-      val oomApplyType = alt.paramLists.head.map(_.typeSignature)
-      oomApplyType.size == oomUnapplyType.size &&
-      oomApplyType.corresponds(oomUnapplyType)(_ =:= _)
-    }
-
-    val applyMethod = optApplyMethod getOrElse {
-      c.abort(
-        c.enclosingPosition, 
-        s"$aType.apply(${oomUnapplyType.map(_.toString).mkString(",")} does not exist)"
-      )
-    }
-    (applyMethod,unapplyMethod)
   }
 
   val tupleNamePattern = "scala\\.Tuple\\d{1,2}".r.pattern
 
   /**
-   * Get the "normalized" types for a type from the unapply method. The return
-   * types of the unapply method are first normalized by discarding the outer
-   * Option. Then if the inner type is a scala.Tuple type then the Tuple type is
-   * discarded and its type parameters are returned. If the inner type is not a 
-   * scala.Tuple type then the inner type is returned unmodified.
+   * Compute the type signature (a non-empty list of (Symbol,Type) pairs) for a
+   * type that can be used to both create and extract a type. The type signature
+   * is used to describe the data contained in the type.
+   *
+   * For case classes, the type signature matches the fields of the case
+   * class. For other types, the type signature equals the first unapply/apply
+   * method pair with matching type signatures in the type's companion object.
+   * Symbol names are extracted from the matching apply method arguments.
+   *
+   * The type signatures of apply/unapply match when either:
+   * 1) The types of the arguments to the apply method equal the option/tuple
+   * extracted return type arguments of the unapply method (extraction here
+   * removes  the Option and Tuple wrappers)
+   * Ex:
+   * case class A(value1: int, value2: Int)
+   * A.apply(value1: Int,value2: Int) "matches" A.unapply(a: A) : Option[(Int,Int)]
+   * 2) The type of the single argument to the apply method matches the
+   * option extracted return type of the unapply method (extraction here removes
+   * only the Option wrapper)
+   * Ex:
+   * case class A(value1: (Int,Int))
+   * A.apply(value1: (Int,Int)) "matches" A.unapply(a: A) : Option[(Int,Int)]
+   *
+   * Correctly computes type signatures of single value case classes,
+   * single value tuple case classes and any other type that declares an
+   * apply/unapply pair with matching type signatures in the type's
+   * companion object.
+   *
    * @param c context
-   * @param unapplyMethod unapply method for the type's companion object
-   * @return non-empty list of normalized unapply return types
+   * @param aType type whose companion object should be searched for apply and
+   *              unapply methods
+   * @return the type signature as a non-empty list of (Symbol,Type) pairs
    */
-  def oomNormType(
+  def calcTypeSig(
     c: blackbox.Context
   )(
-    unapplyMethod: c.universe.MethodSymbol
-  ): List[c.Type] = {
+    aType: c.Type
+  ): List[(c.Symbol, c.Type)] = {
     import c.universe._
-    unapplyMethod.returnType match {
-      // Outer type for unapply is always Option
-      case TypeRef(_, _, outerArgs) if outerArgs.nonEmpty =>
-        outerArgs.head match {
-          // If more than one argument and symbol name matches TupleXX
-          // e.g. Tuple3[A,B,C] => List(A,B,C)
-          case TypeRef(_, symbol, innerArgs)
-            if innerArgs.nonEmpty &
-               tupleNamePattern.matcher(symbol.fullName.toString).matches =>
-            innerArgs
-          // For anything else with inner args, just return the type
-          // e.g. Map[A,B] => Map[A,B]
-          case typeRef@TypeRef(_, _, _) =>
-            List(typeRef)
+
+    val unapplyMethod = getCompanionMethodOrDie(c)(aType,"unapply")
+
+    // Build list of candidate type sigs from unapply method return type sigs
+    val oomCandidateTypeSig =
+      unapplyMethod.returnType match {
+        // Outer type for unapply is always Option
+        case TypeRef(_, _, outerArgs) if outerArgs.nonEmpty =>
+          outerArgs.head match {
+            // If more than one argument and symbol name matches TupleXX then
+            // First try matching apply(_:TupleXX[A,B,..Z]) then apply(A,B,..Z)
+            // e.g. Tuple3[A,B,C] => List(List(Tuple3[A,B,C]),List(A,B,C))
+            case tupleType@TypeRef(_, symbol, innerArgs)
+              if innerArgs.nonEmpty &
+                 tupleNamePattern.matcher(symbol.fullName.toString).matches =>
+              List(List(tupleType), innerArgs)
+            // For anything else just search for apply(_:TypeRef)
+            // e.g. Map[A,B] => List(List(Map[A,B]))
+            case typeRef@TypeRef(_, _, _) =>
+              List(List(typeRef))
+          }
+      }
+
+    val oomApplyMethodAlt =
+      getCompanionMethodOrDie(c)(aType,"apply")
+        .asTerm
+        .alternatives
+        .map(_.asMethod)
+
+    // Search for the first apply method whose type sig matches a candidate
+    // type sig. Note: using stream here to avoid search past first match
+    val optApplyMethod =
+      oomCandidateTypeSig.toStream.flatMap { oomTypeSig =>
+        oomApplyMethodAlt.find { alt =>
+          val oomApplyTypeSig = alt.paramLists.head.map(_.typeSignature)
+          oomApplyTypeSig.size == oomTypeSig.size &&
+          oomApplyTypeSig.corresponds(oomTypeSig)(_ =:= _)
+        } match {
+          case Some(applyMethodAlt) => Some((applyMethodAlt,oomTypeSig))
+          case None => None
         }
-      case _ => c.abort(c.enclosingPosition, "Unreachable")
+      }.headOption
+
+    val (applyMethod,oomTypeSig) = optApplyMethod getOrElse {
+      c.abort(
+        c.enclosingPosition, 
+        s"No apply methods matching unapply: ${oomCandidateTypeSig.map { typeSig =>
+          s"$aType.apply(${typeSig.map(_.toString).mkString(",")})"
+        }.mkString("OR")} )"
+      )
     }
+
+    applyMethod
+      .paramLists
+      .head
+      .zip(oomTypeSig)
   }
+
+
 
 }
